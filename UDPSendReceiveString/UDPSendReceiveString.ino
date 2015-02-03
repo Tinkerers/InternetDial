acc32f512293defdb924a5a6ea2f302657b5b01b
Jacob Sayles <jsayles@gmail.com>
2015-02-03 15:14:01 -0800

    now working

--- UDPSendReceiveString/UDPSendReceiveString.ino
+++ UDPSendReceiveString/UDPSendReceiveString.ino
@@ -17,7 +17,16 @@
 #include <Ethernet.h>
 #include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
 
-long randNumber;
+#include <SwitecX25.h>
+
+// standard X25.168 range 315 degrees at 1/3 degree steps
+#define STEPS (315*3)
+
+// For motors connected to pins 3,4,5,6
+SwitecX25 motor1(STEPS,4,5,6,7);
+
+
+int poll_time = 1000;
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
@@ -25,22 +34,23 @@ byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0xEE, 0x5D };
 
 IPAddress serverIP(172, 16, 5, 1);
 unsigned int serverPort = 161;
-
 unsigned int localPort = 8888;      // local port to listen on
 
 // buffers for receiving and sending data
-byte packetBuffer[57]; //buffer to hold incoming packet,
+byte packetBuffer[59]; //buffer to hold incoming packet,
 
 byte snmp_packet[] = {
-0x30, 0x32, 0x02, 0x01, 0x01, 0x04, 
-0x0d, 0x31, 0x36, 0x31, 0x37, 0x63, 0x30, 0x77, 0x30, 0x72, 0x6b, 0x69, 0x6e, 0x67, 0xa0, 0x1e, 
-0x02, 0x04, 0x25, 0x17, 0x92, 0xce, 0x02, 0x01, 0x00, 0x02, 0x01, 0x00, 0x30, 0x10, 0x30, 0x0e, 
-0x06, 0x0a, 0x2b, 0x06, 0x01, 0x02, 0x01, 0x02, 0x02, 0x01, 0x0a, 0x05, 0x05, 0x00};
+0x30, 0x33, 0x02, 0x01, 0x01, 0x04, 0x0d, 0x31, 0x36, 0x31, 0x37, 0x63, 0x30, 0x77, 0x30, 0x72,
+0x6b, 0x69, 0x6e, 0x67, 0xa0, 0x1f, 0x02, 0x04, 0x10, 0xde, 0x4d, 0x7e, 0x02, 0x01, 0x00, 0x02,
+0x01, 0x00, 0x30, 0x11, 0x30, 0x0f, 0x06, 0x0b, 0x2b, 0x06, 0x01, 0x02, 0x01, 0x1f, 0x01, 0x01,
+0x01, 0x0a, 0x05, 0x05, 0x00
+};
 
 // An EthernetUDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
 
 uint32_t last_octets = 0;
+unsigned long last_time = millis();
 
 void setup() {
   Serial.begin(9600);
@@ -49,6 +59,12 @@ void setup() {
   Ethernet.begin(mac);
   //Ethernet.localIP();
   Udp.begin(localPort);
+  
+  // run the motor against the stops
+  motor1.zero();
+  // start moving towards the center of the range
+  motor1.setPosition(STEPS/2);
+
 }
 
 uint32_t getInOctets() {
@@ -77,61 +93,39 @@ uint32_t getInOctets() {
   return octets_in;
 }
 
+void motor_delay(int ms) {
+  for(int i=0; i < ms; i++) {
+    delay(1);
+    motor1.update();
+  }
+}
+
 void loop() {
-  unsigned long start_ts = millis();
   uint32_t octets = getInOctets();
-  uint32_t delta = octets - last_octets;
-  unsigned long duration = millis() - start_ts;
-  int poll_time = 1000;
+  uint32_t delta_kbytes = (octets - last_octets)/1024;
+  unsigned long duration = millis() - last_time;
+  last_octets = octets;
+  last_time = millis();
   Serial.print("Octets: ");
   Serial.println(octets);
-  Serial.print("delta: ");
-  Serial.println(delta);
+  Serial.print("delta kbytes: ");
+  Serial.println(delta_kbytes);
   Serial.print("KBytes per second: ");
-  Serial.println((delta / duration) * 1000 / 8);
-  delay(poll_time);
-  last_octets = octets;
+  unsigned long kbps = delta_kbytes / (duration/1000);
+  Serial.println(kbps);
+  
+  unsigned long scaled = kbps * 944 / 25000; // 7680 == 60Mbit/s
+  Serial.print("scaled: ");
+  Serial.println(scaled);
+
+  
+  motor1.setPosition(scaled);
+  motor_delay(poll_time);
+
+  
+  
 }
 
 
-/*
-  Processing sketch to run with this example
- =====================================================
- 
- // Processing UDP example to send and receive string data from Arduino 
- // press any key to send the "Hello Arduino" message
- 
- 
- import hypermedia.net.*;
- 
- UDP udp;  // define the UDP object
- 
- 
- void setup() {
- udp = new UDP( this, 6000 );  // create a new datagram connection on port 6000
- //udp.log( true ); 		// <-- printout the connection activity
- udp.listen( true );           // and wait for incoming message  
- }
- 
- void draw()
- {
- }
- 
- void keyPressed() {
- String ip       = "192.168.1.177";	// the remote IP address
- int port        = 8888;		// the destination port
- 
- udp.send("Hello World", ip, port );   // the message to send
- 
- }
- 
- void receive( byte[] data ) { 			// <-- default handler
- //void receive( byte[] data, String ip, int port ) {	// <-- extended handler
- 
- for(int i=0; i < data.length; i++) 
- print(char(data[i]));  
- println();   
- }
- */
 
 
